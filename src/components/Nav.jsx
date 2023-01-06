2011add6f0ee8432f08f6430f6ddfb4422eb3d4c
Anatolii <anatolii.hutsul@ukr.net>
2023-01-06 16:17:37 +0100

    Finished work on the navigation element

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ const Nav = () => {
     return (
         <>
             <header className="header">
-                <h3>React Example</h3>
+                <h3 className="header_title">React Example</h3>
                 <nav>
-                    <NavLink className="headerLink" to="/articles">Home</NavLink>
-                    <NavLink className="headerLink" to="/users">Users</NavLink>
-                    <NavLink className="headerLink" to="/photos">Photos</NavLink>
+                    <NavLink className="header_link" end to="/articles">Home</NavLink>
+                    <NavLink className="header_link" to="/users">Users</NavLink>
+                    <NavLink className="header_link" to="/photos">Photos</NavLink>
                 </nav>
             </header>
             <Outlet/>
